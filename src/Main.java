ebb2e97fc0cb15e2c393c08381b877e84ede12fa
pensuke <sho.pensuke@gmail.com>
2023-08-31 19:47:31 +0900

    add arrayList

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,24 @@
 package classes;
 import classes.park.*;
 import classes.attraction.*;
+import java.util.*;
 
 class Main{
     public static void main(String[] args){
         Area area = Area.AdventureLand;
         String areaName = area.getAreaName();
 
-        Attraction[] attAry = new Attraction[7];
+        List<Attraction> attList = new ArrayList<>();
 
-        attAry[0] = new DPAAttraction(
-                        "ソアリン",
-                        "MediterraneanHarbor",
-                        87
-                    );
-        DPAAttraction soaring = (DPAAttraction)attAry[0];
+        attList.add(new DPAAttraction("ソアリン", "MediterraneanHarbor", 87));
+        attList.add(new DPAAttraction("スプラッシュマウンテン", "CritterCountry", 8));
+        attList.add(new DPAAttraction("美女と野獣", "FantasyLand", 10));
+        attList.add(new PriorityAttraction("スペースマウンテン", "TomorrowLand", 12));
+        attList.add(new PriorityAttraction("ビッグサンダーマウンテン", "WesternLand", 30));
+        attList.add(new Attraction("シンド", "ArabianCoast", 24));
+        attList.add(new Attraction("スターツアーズ", "TomorrowLand", 40));
 
-        attAry[1] = new DPAAttraction(
-                        "スプラッシュマウンテン",
-                        "CritterCountry",
-                        8
-                    );
-        DPAAttraction splash = (DPAAttraction)attAry[1];
-
-        attAry[2] = new DPAAttraction(
-                        "美女と野獣",
-                        "FantasyLand",
-                        10
-                    );
-        DPAAttraction ebb = (DPAAttraction)attAry[2];
-
-        attAry[3] = new PriorityAttraction(
-                        "スペースマウンテン",
-                        "TomorrowLand",
-                        12
-                    );
-        PriorityAttraction space = (PriorityAttraction)attAry[3];
-
-        attAry[4] = new PriorityAttraction(
-                        "ビッグサンダーマウンテン",
-                        "WesternLand",
-                        30
-                    );
-        PriorityAttraction big = (PriorityAttraction)attAry[4];
-
-        attAry[5] = new Attraction(
-                        "シンド",
-                        "ArabianCoast",
-                        24
-                    );
-        Attraction sindo = attAry[5];
-
-        attAry[6] = new Attraction(
-                        "スターツアーズ",
-                        "TomorrowLand",
-                        40
-                    );
-        Attraction star = attAry[6];
-
-        for(Attraction att : attAry){
+        for(Attraction att : attList){
             att.printInfo();
             System.out.println("====");
         }
